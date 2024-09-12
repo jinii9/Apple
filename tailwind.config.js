235e0656fac8d75d32ed1d38dd8238038840555d
jinii9 <jinii915@naver.com>
2024-09-12 15:31:57 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1300px",
+      },
+    },
+    extend: {
+      colors: {
+        back: "#F5F5F7",
+        content: "#1D1D1F",
+        gray01: "6E6E73",
+      },
+    },
+  },
+  plugins: [],
+};
