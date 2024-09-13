e1b98b69ce5afbbf2fae432d8c7dcb211171fe52
friend5hip <jwy97@naver.com>
2024-09-13 10:33:49 +0900

    Test commit

---
+++ src/components/productdetail/components/LocalNavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function LocalNavBar() {
+  return (
+    <div className="container">
+      <a href="#">악세사리</a>
+      <div>
+        <span>모두 보기</span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
+
+export default LocalNavBar;
---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LocalNavBar from "../components/LocalNavBar";
+
+function ProductDetailPage() {
+  return <LocalNavBar />;
+}
+
+export default ProductDetailPage;
