534e56896f9ad523a1f09b66cdbb6e75574b3386
jinii9 <jinii915@naver.com>
2024-09-14 23:12:11 +0900

    design: header 애니메이션

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useHeaderColor } from "../context/HeaderContext";
 import { extractOpacity } from "../utils/colorUtils";
+import "../assets/styles/header.css";
 
 function HeaderMenu({ hoveredMenu, data }) {
   const { headerColor } = useHeaderColor();
@@ -11,13 +12,17 @@ function HeaderMenu({ hoveredMenu, data }) {
   return (
     <>
       <div
-        className="absolute z-10 w-full py-14"
+        className="menuWrap absolute z-10 w-full py-14"
         style={{ backgroundColor: menuBackColor }}
       >
-        <div className="menuWrap flex gap-12 max-w-5xl mx-auto pt-11">
+        <div className="flex gap-12 max-w-5xl mx-auto pt-11">
           {menuItems.length > 0 &&
             menuItems.map((subMenu, i) => (
-              <div key={i} style={{ color: headerColor.textColor }}>
+              <div
+                key={i}
+                style={{ color: headerColor.textColor }}
+                className="menuItemWrap"
+              >
                 <h3 className="text-xs text-gray01 mb-4">{subMenu.Title}</h3>
                 <ul className="flex flex-col gap-2">
                   {subMenu.items.map((item, idx) => (
