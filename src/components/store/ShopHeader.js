7be74994633af243e0e1d15265b26abab3f6bfdb
pjm2571 <pjm2571@naver.com>
2024-09-19 22:01:30 +0900

    feat: Shop Header 반응형 제외 구현 완료

--- src/components/store/ShopHeader.js
+++ src/components/store/ShopHeader.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 
+import chatImage from "../../assets/images/store/chat-icon.jpeg";
+import nearImage from "../../assets/images/store/apple-gps-icon.jpg";
+
 function ShopHeader() {
   const [containerStyle, setContainerStyle] = useState({});
 
@@ -26,7 +29,52 @@ function ShopHeader() {
       className="mx-[22px] min-h-[196px] relative w-auto lg:mx-[140px]"
       style={containerStyle}
     >
-      Shop
+      <div className="grid grid-cols-12 gap-4 relative">
+        <div className="shop-header-section col-span-8 max-w-[640px] py-[80px] pb-[64px]">
+          <h1 className="rs-shop-header text-[48px] font-semibold leading-[1.0835] tracking-tight inline text-black">
+            스토어.
+          </h1>
+          <div className="rs-shop-subheader text-[48px] font-semibold leading-[1.0835] tracking-tight inline text-gray-500">
+            좋아하는 Apple 제품을 구입하는 가장 좋은 방법.
+          </div>
+        </div>
+        <div className="shop-chatstore-section col-span-4 absolute right-0 flex flex-col justify-end py-[90px] pb-[74px]">
+          <div id="generic-2">
+            <div className="rf-shop-chat-section flex flex-row text-[14px] font-normal leading-[1.4286] tracking-tight pb-4">
+              <div className="rf-shop-chaticon-container w-[36px] h-[36px] mr-2 ">
+                <img
+                  src={chatImage}
+                  alt="chat"
+                  className="w-full h-full object-contain rounded-full"
+                />
+              </div>
+              <div className="rf-shop-chat-container">
+                <div className="rf-shop-chat-title font-bold">
+                  쇼핑 지원이 필요하다면?
+                </div>
+                <div className="rf-shop-chat-content text-blue-400 cursor-pointer hover:underline">
+                  스페셜리스트에게 문의하세요 ↗
+                </div>
+              </div>
+            </div>
+            <div className="rs-shop-neareststore flex flex-row text-[14px] font-normal leading-[1.4286] tracking-tight pb-4">
+              <div className="rf-shop-storeicon-container w-[36px] h-[36px] mr-2">
+                <img
+                  src={nearImage}
+                  alt="chat"
+                  className="w-full h-full object-contain"
+                />
+              </div>
+              <div>
+                <div className="font-bold">Apple Store를 방문하세요</div>
+                <div className=" text-blue-400 cursor-pointer hover:underline">
+                  가까운 매장 찾기 〉
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/StorePage.js
+++ src/pages/StorePage.js
@@ -8,11 +8,28 @@ function StorePage() {
   useEffect(() => {
     setHeaderBackColor("rgba(22, 22, 23, .8)");
     setHeaderTextColor("#fff");
-  }, []);
+  }, [setHeaderBackColor, setHeaderTextColor]);
+
+  const bgStyle = {
+    backgroundColor: "#f5f5f7",
+  };
+
+  const fontStyleKo = {
+    fontFamily: `"SF Pro KR", "SF Pro Display", "SF Pro Icons", "Apple Gothic", "HY Gulim", "MalgunGothic", "HY Dotum", "Lexi Gulim", "Helvetica Neue", Helvetica, Arial, sans-serif`,
+    letterSpacing: 0,
+    lineHeight: "1.1875",
+    wordBreak: "keep-all",
+    display: "inline",
+  };
+
+  // 스타일 객체 병합
+  const combinedStyle = { ...fontStyleKo, ...bgStyle };
 
   return (
     <>
-      <ShopHeader />
+      <div style={combinedStyle}>
+        <ShopHeader />
+      </div>
     </>
   );
 }
