abea1631ed384aac99ecd9c54540078eed190a87
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-09-20 15:54:37 +0900

    chore: 머지전 업로드요

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Map } from "react-kakao-maps-sdk";
+import "../assets/map.css"
 
 function MapPage() {
   return (
-    <div>
-      <div className="flex justify-center font-extrabold">
-        <h1 className='mb-3 text-3xl' style={{ fontFamily: "" }}>매장 찾기</h1>
+    <div className='bg-gray-100'>
+      <div className="flex justify-center font-medium">
+        <h1 className='mb-3 text-4xl'>근처 매장</h1>
         </div>
     <div className="container px-5 mx-auto ">
       <Map
-        center={{ lat: 36.450701, lng: 126.570667 }}
+        center={{ lat: 37.440701, lng: 126.970667 }}
         className="border-4 rounded-3xl w-full h-96 sm:h-80 md:h-96 lg:h-[500px] xl:h-[600px]" 
         level={3}
       />  
