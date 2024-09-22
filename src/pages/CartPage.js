b60bc1cd0200eff556615609bd26b82796d23c64
jinii9 <jinii915@naver.com>
2024-09-22 15:33:56 +0900

    design: 장바구니 총액ui 퍼블리싱

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,32 +5,27 @@ const cartItems = [{}];
 function CartPage() {
   return (
     <>
-      {/* <div className="mainWrap pt-12 mx-64 mb-24"> */}
-
       {/* container 대신에 max-w-5xl */}
       <div className="mainWrap max-w-5xl mx-auto pt-12 border">
-        <div className="mx-3">
-          <div className="flex flex-col items-start">
+        {/* 총액 */}
+        <div className="totalWrap bg-red-50 flex items-center justify-center">
+          <div className="pt-2 pb-5 text-center">
             <h2 className="text-4xl font-bold leading-10">
-              장바구니가 비어 있습니다.
+              장바구니 총액: ₩1231231
             </h2>
             <p className="text-lg leading-7 mt-3">
               모든 주문에 무료 배송 서비스가 제공됩니다.
             </p>
-          </div>
-          <div className="mt-8">
-            <button className="py-4 px-36 border border-blue01 rounded-xl text-blue01">
-              쇼핑 계속하기
-            </button>
+            <div className="mt-8">
+              <button className="w-72 py-2 px-4 border border-blue01 rounded-lg bg-blue01 text-white">
+                <span>결제</span>
+              </button>
+            </div>
           </div>
         </div>
+        {/* 카트 */}
+        <div className="cartWrap"></div>
       </div>
-
-      <div className="inquiryWrap mt-12 border-t border-b">
-        <p></p>
-      </div>
-      <div className="imgWrap"></div>
-      <div className="accordionWrap"></div>
     </>
   );
 }
