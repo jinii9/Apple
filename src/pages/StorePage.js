9a998555806a310928b932938c763462abd41d74
pjm2571 <pjm2571@naver.com>
2024-09-22 14:05:48 +0900

    feat: cardsShelf6 구현

--- src/pages/StorePage.js
+++ src/pages/StorePage.js
@@ -7,6 +7,7 @@ import ShopCardsShelf2 from "../components/store/ShopCardsShelf2";
 import ShopCardsShelf3 from "../components/store/ShopCardsShelf3";
 import ShopCardsShelf4 from "../components/store/ShopCardsShelf4";
 import ShopCardsShelf5 from "../components/store/ShopCardsShelf5";
+import ShopCardsShelf6 from "../components/store/ShopCardsShelf6";
 
 function StorePage() {
 
@@ -36,6 +37,7 @@ function StorePage() {
         <ShopCardsShelf3 />
         <ShopCardsShelf4 />
         <ShopCardsShelf5 />
+        <ShopCardsShelf6 />
       </div>
     </>
   );
