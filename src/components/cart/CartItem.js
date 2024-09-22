bbee7cbef2608b345501b834b61150ee7efe5800
jinii9 <jinii915@naver.com>
2024-09-23 03:55:03 +0900

    feat: 장바구니 reducer 추가

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,9 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as ARROW } from "../../assets/images/cart/arrow.svg";
+import { ReactComponent as CART } from "../../assets/images/cart/cart.svg";
+import { useDispatch } from "react-redux";
+import { addComma } from "../../utils/priceFormat";
 
 function CartItem({ item }) {
-  console.log(item);
+  const imageUrl = require("../../assets/images/iPhone_16_t_case/MA6A4.jpg");
+  const dispatch = useDispatch();
+  const [seletedCnt, setSelectedCnt] = useState(1);
 
-  return <div>CartItem</div>;
+  const handleRemoveBtn = (item) => {
+    console.log(item);
+    dispatch({ type: "REMOVE_ITEM", payload: item });
+  };
+
+  const handleSelectChange = (e) => {
+    const cnt = parseInt(e.target.value);
+    setSelectedCnt(cnt);
+
+    dispatch({
+      type: "UPDATE_CNT",
+      payload: { id: item.id, count: cnt },
+    });
+  };
+
+  return (
+    <div className="py-20" style={{ borderTop: "1px solid #D2D2D7" }}>
+      <div className="flex">
+        {/* 이미지 */}
+        <div className="max-w-[203px] flex-1">
+          <img
+            className="w-full h-full object-cover"
+            src={imageUrl}
+            alt="상품 이미지"
+          />
+        </div>
+
+        {/* 설명 */}
+        <div className="flex-1">
+          {/* 설명1 */}
+          <div className="flex">
+            <div className="w-[50%] pr-5">
+              <h1 className="font-bold text-2xl ">{item.product_name}</h1>
+            </div>
+
+            <div className="w-[14%] pl-5">
+              <div className="inline-block relative text-2xl font-bold ">
+                <select
+                  className="appearance-none bg-transparent pr-10 cursor-pointer"
+                  value={seletedCnt}
+                  onChange={handleSelectChange}
+                >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+
+                <span className="absolute right-3 transform top-1/2 -translate-y-1/2 pointer-events-none">
+                  <ARROW
+                    width="20"
+                    height="20"
+                    stroke="#0071e3"
+                    strokeWidth="14"
+                  />
+                </span>
+              </div>
+            </div>
+
+            <div className="w-[36%] flex flex-col items-end gap-3 font-semibold">
+              <h2>₩{addComma(item.price)}</h2>
+              <button
+                className="block text-blue01"
+                onClick={() => handleRemoveBtn(item)}
+              >
+                삭제
+              </button>
+              <button className="block text-blue01">나중을 위해 저장</button>
+            </div>
+          </div>
+
+          {/* 설명2: 픽업 */}
+          <div className="flex ml-2">
+            <span>
+              <CART />
+            </span>
+            <p className="ml-2">가까운 Apple Store에서 픽업하십시오.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartItem;
